f52e33fb2b6314b3f84a78028c8a635ea3155ea9
ParanormalRave <147731636+ParanormalRave@users.noreply.github.com>
2026-01-17 12:46:46 +0100

    Add files via upload

--- to-do-list_CLI.cpp
+++ to-do-list_CLI.cpp
@@ -6,42 +6,50 @@
 #include <vector>
 using namespace std;
 
-void addTask(vector<string>task);
+vector<string>tasks;
+void addTask(string task);
+
 int main(){
-    cout <<  "Please enter a number to perform a task:\n 1. Add Task \n 2. View Task \n 3. Mark task as done \n 4. Delete Task 5. Exit ";
+    cout <<  "Please enter a number to perform a task:\n 1. Add Task \n 2. View Task \n 3. Mark task as done \n 4. Delete Task \n 5. Exit \n\n";
     int number{};
     cin >> number;
 
-    vector<string> task;
-    switch (number) {
-    case 1:
-        cin << task;
-        addTask(task);
-        break;
-    case 2:
+    //switch (number) {
 
-        break;
-    case 3:
+    //case 1:
+    //    cin >> task;
+    //    addTask(task);
+    //    break;
+    //case 2:
 
-        break;
-    case 4:
+    //    break;
+    //case 3:
 
-        break;
-    default:
-        cout << 'hmm';
-    }
+    //    break;
+    //case 4:
+
+    //    break;
+    //default:
+    //    cout << 'hmm';
+    //}
+
+   if (number == 1) {
+       cout << "Enter the task \n";
+       string task;
+       cin >> task;
+       addTask(task);
+   }
     return 0;
 }
 
-void addTask(vector<string>& task) {
-    vector<string>tasks{};
+void addTask(string task) {
     tasks.push_back(task);
+    cout << "You have successfully added a task";
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
+// Run program: Ctrl + F5 or Debug > S
 // Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
+//   1. Use the Solution Explorer window ttart Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
